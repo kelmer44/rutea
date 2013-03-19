3318e0c43de3db9b72d87be2d7c6216ec097ea6d
kelmer <kelmer@gmail.com>
2013-03-19 10:57:14 +0100

    Boton play

--- src/com/bretema/rutas/view/fragment/VideoFragment.java
+++ src/com/bretema/rutas/view/fragment/VideoFragment.java
@@ -1,29 +1,15 @@
 package com.bretema.rutas.view.fragment;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -40,11 +26,16 @@ public class VideoFragment extends MultimediaFragment {
 	// NO reproducimos si el video no se ha hallado o no funciona
 	private boolean							doPlay	= true;
 
+	private ImageView playButton;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.video_details, container, false);
 		String imageURI = getMultimedia().getUri();
 		String videoCaption = getMultimedia().getDescripcion();
+		
+		playButton = (ImageView) view.findViewById(R.id.play_button);
+		
 		TextView textView = (TextView) view.findViewById(R.id.imageCaptionSlideshow);
 		mVideoView = (VideoView) view.findViewById(R.id.videoViewSlideShow);
 
@@ -84,6 +75,19 @@ public class VideoFragment extends MultimediaFragment {
 			}
 
 		});
+		
+		//icono de play sobre el vídeo
+		playButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(!mVideoView.isPlaying()){
+					mVideoView.start();
+				}
+				playButton.setVisibility(View.INVISIBLE);
+			}
+		});
+		
 		textView.setText(videoCaption);
 		return view;
 	}
@@ -103,6 +107,7 @@ public class VideoFragment extends MultimediaFragment {
 				mVideoView.seekTo(0);
 			}
 			mMedia.hide();
+			playButton.setVisibility(View.VISIBLE);
 		}
 	}
 
